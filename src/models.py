2051ba09e1cfacba4cc5840a4fbe99a017e1e6ec
Jesus Lara <jelara99@gmail.com>
2021-04-07 03:34:36 +0000

    modelo de base de datos

--- src/models.py
+++ src/models.py
@@ -8,23 +8,39 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+class Follower(Base):
+    __tablename__ = 'Follower'
+    id = Column(Integer, primary_key=True)
+    User_to_id = Column(Integer, ForeignKey('User.id'))
+    User_from_id = Column(Integer, ForeignKey('User.id'))
+
+class Post(Base):
+    __tablename__ = 'Post'
+    id = Column(Integer, primary_key=True)
+    User_id = Column(Integer, ForeignKey('User.id'))
+
+class Media(Base):
+    __tablename__ = 'Media'
+    id = Column(Integer, primary_key=True)
+    Type = Column(Integer)
+    url = Column(String(250))
+    Post_id= Column(Integer, ForeignKey('Post.id'))
+
+class Coment(Base):
+    __tablename__ = 'Coment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    Author_id= Column(Integer, ForeignKey('Post.id'))
+    User_id = Column(Integer, ForeignKey('User.id'))
 
     def to_dict(self):
         return {}
